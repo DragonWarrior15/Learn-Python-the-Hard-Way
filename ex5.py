8f016cb646f475b412944882e2bd3459ef42158f
DragonWarrior15 <DragonWarrior15@users.noreply.github.com>
2016-11-11 16:43:37 +0530

    update file for ex5

--- ex5.py
+++ ex5.py
@@ -1,4 +1,19 @@
+# -*- coding: utf-8 -*-
+
 my_name = 'Dragon Warrior'
 my_age = 1000
 my_height = 180 # cm
 my_weight = 70 # kg
+my_eyes= 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d cms tall." % my_height
+print "He's %d kgs heavy." % my_weight
+print "Actually tha's not too heavy."
+print "He's got %s eyes and %s hair." %(my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# a tricky one
+print "If I add %d, %d, and %d I get %d." %(my_age, my_height, my_weight, my_age + my_height + my_weight)
